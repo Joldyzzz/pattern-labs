bd80cb4f8ed3e33a91212be7437662e39820fbd0
Joldyzzz <joldyzzz@mail..ru>
2025-05-13 20:25:47 +0300

    Добавил паттерн Прототип (Prototype)

---
+++ src/Creational/Prototype/ColorPoint.java
@@ -0,0 +1,30 @@
+package Creational.Prototype;
+
+public class ColorPoint extends Point implements Prototype {
+    StringBuilder color;
+
+    public ColorPoint(int x, int y, String color) {
+        super(x, y);
+        this.color = new StringBuilder(color);
+    }
+
+    public ColorPoint(int x, int y, StringBuilder color) {
+        super(x, y);
+        this.color = color;
+    }
+
+    public ColorPoint(ColorPoint p) {
+        this(p.x, p.y, new StringBuilder(p.color)); // deep clone
+    }
+
+    @Override
+    public ColorPoint Clone() {
+        return new ColorPoint(this);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Color Creational.Prototype.Point (%d,%d) %s", x, y, color);
+    }
+
+}
---
+++ src/Creational/Prototype/Point.java
@@ -0,0 +1,26 @@
+package Creational.Prototype;
+
+public class Point implements Cloneable {
+
+    int x, y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point(Point p) {
+        this(p.x, p.y);
+    }
+
+    @Override
+    public Point clone() {
+        return new Point(this);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Creational.Prototype.Point (%d,%d)", x, y);
+    }
+
+}
