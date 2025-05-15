1c6e59982c61a894e8bd89f0f0882b9dd0ae4741
Joldyzzz <joldyzzz@mail..ru>
2025-05-15 08:55:03 +0300

    Добавил пример по паттерну Компоновщик (Composite)

---
+++ src/Structural/Composite/widgets/UIComponent.java
@@ -0,0 +1,10 @@
+package Structural.Composite.widgets;
+
+public abstract class UIComponent {
+    public abstract boolean draw(int line);
+
+    public abstract int getHeight();
+
+    public abstract int getWidth();
+
+}
